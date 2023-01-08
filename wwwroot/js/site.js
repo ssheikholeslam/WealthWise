13ba0c1a394f2d89a5a5bd5cefc70b19245688f8
ssheikholeslam <88804397+ssheikholeslam@users.noreply.github.com>
2023-01-08 18:17:08 -0500

    💄 Home Page Design
    Adds the initial design of guest user home page.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,4 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-import 'animate.css';
+
+// Hero demo scroll animation intersection listener
+let heroGradientElement;
+let heroGlowElement;
+let heroImageElement;
+let sketchLinesSVG;
+
+window.addEventListener("load", (event) => {
+    heroGradientElement = document.querySelector('.hero-gradient');
+    heroGlowElement = document.querySelector('.hero-glow');
+    heroImageElement = document.querySelector('.hero-image');
+    sketchLinesSVG = document.querySelector('.sketch-lines');
+
+    createObserver();
+}, false);
+
+function createObserver() {
+    let observer;
+
+    let options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.4
+    };
+
+    observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(heroGradientElement);
+}
+
+function handleIntersect(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio >= 0.4) {
+            heroGradientElement.style.animation = 'rotate-image 1400ms ease forwards';
+            heroGlowElement.style.animation = 'image-glow 1400ms 680ms ease forwards'; 
+            sketchLinesSVG.style.animation = 'sketch-lines 1200ms ease-out forwards';
+            heroImageElement.style.animation = 'image-fade 1400ms 500ms ease-in forwards';
+        } 
+    });
+}
