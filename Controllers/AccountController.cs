a8aab229ff83642e6110ed64b1aa12df6e3a908c
ssheikholeslam <88804397+ssheikholeslam@users.noreply.github.com>
2023-01-09 22:12:42 -0500

    💄 Redesign Login Page
    Redesigns user login page. Functionality still WIP.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WealthWise.Models;
+
+namespace WealthWise.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
